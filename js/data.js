215d9c1c4a5426c403b832a4c0c20c1599254a6e
Volf <Volf@wol>
2016-03-17 05:56:13 +0200

    .

---
+++ js/data.js
@@ -0,0 +1,45 @@
+var localeData = {
+    "type": "FeatureCollection",
+
+    "features": [{
+        "type": "Feature",
+        "properties": {
+            "color": "red",
+            "id": "1, 1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [30.55572509765625, 50.440014664018136]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "color": "blue",
+            "id": "1, 1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [30.44220118709643, 50.537185668945312 ]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "color": "green",
+            "id": "1, 1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [30.61065673828125, 50.424706174038526 ]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "color": "purple",
+            "id": "1, 1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [30.45135498046875, 50.450509053586615]
+        }
+    }]
+}
